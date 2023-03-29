4a02859682592dd0ec1e9bc709ea8267842af3a8
Isidora <isidora.oyanedel@usach.cl>
2023-03-29 16:15:29 -0300

    estres

---
+++ TDA_SYSTEM_CONSTRUCTOR.rkt
@@ -0,0 +1,86 @@
+#lang racket
+;Isidora Oyanedel
+#|TDA System - CONSTRUCTOR
+-----------descripcion del TDA--------------
+Función constructora de un sistema. Deja registro de la fecha de creación.
+
+          --- CONSTRUCTOR ---
+DOMINIO: string
+RECORRIDO: system (conformado por nombre sistema, drive y usuario/s)
+DESCRIPCION: se ingresa un string en la funcion devolviendo un system
+             conformado por una lista de elementos que complementan el system
+USO DE RECURSION: no se usa.
+
+debe tener el nombre system y funcionar con las cosas del doc
+
+probablemente se usa el TDA FECHA
+|#
+
+;(define (system string) 
+ ; (list string 16042023))
+
+(define (system string)
+  (make-system string null null))  ;el primer elemento es el nombre, siguiente drive, sig usuario
+
+
+(define (make-system nombre drive usuario)
+  (list nombre drive usuario))
+
+#|FUNCION RUN
+
+DOMINIO: System X Command (funcion =command)
+RECORRIDO: system
+DESCRIPCION: Función que permite ejecutar un comando (función)
+sobre un sistema. Toda acción realizada con run relativa a creación de archivos,
+carpetas, renombrar, copiar, mover, eliminar, debe dejar un registro de la fecha de
+modificación, además de verificar los permisos del recurso que será alterado|#
+
+(define (run system command) ;se aplica una funcion en la lista system por ejemplo add-rive
+  (command system))
+
+#|Otras funciones necesarias|#
+(define get-system-name car)
+(define get-system-drive cadr)
+(define get-system-usuarios caddr)
+
+#|FUNCION ADD-DRIVE seguro hay que cambiarlo a otro archivo
+DOMINIO: system x/dominio del primer lambda
+         letter(char) x name(string) x capacity(int) /este se refiere a el dominio de la currificacion
+RECORRIDO: system
+DESCRIPCION: Función que permite añadir una unidad a un
+sistema. La letra de la unidad es única.
+RECURSION: no
+
+|#
+
+(define (make-drive letter name capacity)
+  (list letter name capacity))
+                    
+(define add-drive
+  (lambda(system)
+    (lambda (letter name capacity) ;info del drive
+      (make-system (get-system-name system)
+                   (cons(make-drive letter name capacity) ;;make-drive= lista que recibe 3 cosas, y le agrega algo adelante de 3 cosas
+                        (get-system-drive system)) ;cadr de la lista system, system =lista
+      (get-system-usuarios system)))))
+
+
+
+
+
+
+
+
+;ejemplos
+(define S0 (system "newSystem"))
+
+;S1
+;S0
+
+(define S1 ((run S0 add-drive) #\C "SO1" 3000))
+(define S2 ((run S1 add-drive) #\C "SO1" 3000))
+(define S3 ((run S2 add-drive) #\D "Util" 2000))
+
+
+
+(provide (all-defined-out)) ;es para que las funciones de aqui se usen en otros
--- tda usuario.rkt
+++ tda usuario.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require "TDA_SYSTEM_CONSTRUCTOR.rkt")
 #| TDA USUARIO
 primero se declarará la variable user donde tendrá su nombre, el cual será irrepetible,
 se tratará de hacer funcion donde se determine si un usuario es igual a otro
@@ -9,34 +10,55 @@ sistema. El nombre de usuario es único y no puede ser duplicado.
 Dominio: system x username (string)
 Recorrido: system
 RECURSION: NO
-
+ideas: se necesitan las funciones creadas en tda system(nombre temporal) para poder registrar un usuario en lista usuarios
 |#
 
-(define (register user) ;se supone que registra el usuario
-  (list user))       ;registra el usuario en una lista, pero deberia crear una funcion que tome la lista
-                     ;formada aqui y haga una lista de listas
+(define (make-user string) ;se supone que crea el usuario
+  (list string))   
+
+(define get-usuarios car);obtener usuarios
+
+
+(define add-user
+  (lambda(system)
+    (lambda (string)
+      (if(member string (map car(get-system-usuarios system))) ;buen
+         ((display "ya existe el usuario")(display string)system) ;if
+         (make-system(get-system-name system);else
+                     (get-system-drive system)
+                     (cons(make-user string)(get-system-usuarios system)))))))
+           
 
 #|FUNCION PARA VERIFICAR SI EL USUARIO SE REGISTRÓ
 Pertenencia: verifica si el usuario ya existia o no
 DESCRIPCION DE LA FUNCION: funcion ligada a la no duplicidad de nombres de usuario
 dominio: lista
 recorrido: boolean
+recursion: no
 |#
 
 (define (is-user? user)
-  (if(list? user)
-     (if(string?(car user))
+  (if(list? user) ;pregunta si user es lista, en este caso s4 lo es
+     (if(string?(car(car(car(reverse user)))))
         #t   ;if
         #f) ;else
      #f))
 
 
 
-(define user0 (register "isi")) ;prueba del registro
-(is-user? user0) ;prueba de si es user
 
-(define user1 (register "Dominic"))
-(is-user? user1)
 
+
+(define S4 ((run S3 add-user) "user1"))
+S4
+(define S5 ((run S3 add-user) "user1"))
+;(is-user? S4)
+S5
+(define S6 ((run S3 add-user) "user2"))
+S6
+
+;(map string? S4);para saber si hay un string en la lista
+;(map string? (car(car(reverse S4)))); para saber si hay string en la lista usuarios
+;(member "user1" (map car(car(reverse S4)))) ;verifica si en la lista está cierto usuario
 
 
